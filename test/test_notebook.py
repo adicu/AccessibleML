1ffd8c7e35a019c598afbbbfc05ecd22368bfa6d
Alan Du <alanhdu@gmail.com>
2016-02-06 18:10:28 -0500

    [CI] Ensure notebooks cleared before commit
    Clearing the notebooks makes the git diffs _much_ nicer. Better code
    review, fewer merge conflicts => good things.

--- test/test_notebook.py
+++ test/test_notebook.py
@@ -14,3 +14,14 @@ def test_notebook_execution(notebook):
 
     ep = ExecutePreprocessor(timeout=600, kernel_name="python3")
     ep.preprocess(nb, resources={})
+
+
+@pytest.mark.parametrize("notebook", notebooks)
+def test_notebook_clear(notebook):
+    with open(notebook) as fin:
+        nb = nbformat.read(fin, as_version=4)
+
+    for cell in nb.cells:
+        if cell["cell_type"] == "code":
+            assert cell["outputs"] == []
+            assert cell["execution_count"] is None
