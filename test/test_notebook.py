15c12f523d9b609a2de22541aa5e24d0cee3def0
Alan Du <alanhdu@gmail.com>
2016-02-06 16:09:40 -0500

    Up timeout length to 600 seconds / notebook

--- test/test_notebook.py
+++ test/test_notebook.py
@@ -12,5 +12,5 @@ def test_notebook_execution(notebook):
     with open(notebook) as fin:
         nb = nbformat.read(fin, as_version=4)
 
-    ep = ExecutePreprocessor(timeout=120, kernel_name="python3")
+    ep = ExecutePreprocessor(timeout=600, kernel_name="python3")
     ep.preprocess(nb, resources={})
