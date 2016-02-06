ff65d518bd7cf4a34364c949f218fa98c6d35693
Alan Du <alanhdu@gmail.com>
2016-02-05 23:25:02 -0500

    Update test

--- test/test_notebook.py
+++ test/test_notebook.py
@@ -1,26 +1,16 @@
-import os
 import glob
-import contextlib
-import subprocess
 
+from nbconvert.preprocessors import ExecutePreprocessor
+import nbformat
 import pytest
 
 notebooks = list(glob.glob("*.ipynb", recursive=True))
 
-@contextlib.contextmanager
-def cleanup(notebook):
-    name, __ = os.path.splitext(notebook)
-    yield
-
-    fname = name + ".html"
-    if os.path.isfile(fname):
-        os.remove(fname)
-
 
 @pytest.mark.parametrize("notebook", notebooks)
-def test_notebook(notebook):
-    with cleanup(notebook):
-        # hack to execute the notebook from commandline
-        assert 0 == subprocess.call(["jupyter", "nbconvert", "--to=html",
-                                     "--ExecutePreprocessor.enabled=True",
-                                     notebook])
+def test_notebook_execution(notebook):
+    with open(notebook) as fin:
+        nb = nbformat.read(fin, as_version=4)
+
+    ep = ExecutePreprocessor(timeout=120, kernel_name="python3")
+    ep.preprocess(nb, resources={})
