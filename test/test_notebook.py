a020645799f03de77c8d59d98c0d3be541376ac7
Alan Du <alanhdu@gmail.com>
2016-02-06 19:29:58 -0500

    Increase timeout from 600 -> 1200
    Why, oh why are Travis OS X and AppVeyor so slow?

--- test/test_notebook.py
+++ test/test_notebook.py
@@ -12,7 +12,7 @@ def test_notebook_execution(notebook):
     with open(notebook) as fin:
         nb = nbformat.read(fin, as_version=4)
 
-    ep = ExecutePreprocessor(timeout=600, kernel_name="python3")
+    ep = ExecutePreprocessor(timeout=1200, kernel_name="python3")
     ep.preprocess(nb, resources={})
 
 
