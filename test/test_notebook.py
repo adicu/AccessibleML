b60bcae61d2da4a6869db25f233e8bec40740ffc
Alan Du <alanhdu@gmail.com>
2016-03-10 14:50:12 -0500

    Remove requirement to clear notebooks

--- test/test_notebook.py
+++ test/test_notebook.py
@@ -14,14 +14,3 @@ def test_notebook_execution(notebook):
 
     ep = ExecutePreprocessor(timeout=2400, kernel_name="python3")
     ep.preprocess(nb, resources={})
-
-
-@pytest.mark.parametrize("notebook", notebooks)
-def test_notebook_clear(notebook):
-    with open(notebook) as fin:
-        nb = nbformat.read(fin, as_version=4)
-
-    for cell in nb.cells:
-        if cell["cell_type"] == "code":
-            assert cell["outputs"] == []
-            assert cell["execution_count"] is None
